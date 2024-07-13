cdb208372b47910ff5ad4751885ef82fcd6d3ec9
Ryan Fitmawan <llyodwastaken@gmail.com>
2024-07-13 18:53:30 +0700

    Add files via upload

---
+++ growtopia/server_data.php
@@ -0,0 +1,11 @@
+server|45.77.32.178
+port|17091
+type|1
+#maint|Server currently change hosting, please join discord.gg/gtps15 to get the latest host.
+
+beta_server|127.0.0.1
+beta_port|48801
+
+beta_type|1
+meta|localhost
+RTENDMARKERBS1001
